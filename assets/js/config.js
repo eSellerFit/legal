a184b367b50811d962b86f475cd6b22ab945f862
eSellerFit <giorgi.chakhidze@gmail.com>
2026-03-27 20:24:10 -0400

    Delete assets/js/config.js

--- assets/js/config.js
+++
@@ -1,4 +0,0 @@
-window.ESF_CONFIG = {
-  webAppUrl: 'https://script.google.com/macros/d/AKfycbzGxzKr_9opy2ZdnJQJ_91GmnR3K3OUqr0_YAsVicNHukMZvZZBrUpfG0nYdMf25bnb/useTrigger',
-  bookingUrl: 'https://calendly.com/gio_chakhidze/new-meeting'
-};
