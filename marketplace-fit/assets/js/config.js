37312439dfd8c0bf93d3abf4995e1d1aa29f3690
eSellerFit <giorgi.chakhidze@gmail.com>
2026-03-28 13:30:19 -0400

    Update config.js

--- marketplace-fit/assets/js/config.js
+++ marketplace-fit/assets/js/config.js
@@ -1,4 +1,6 @@
 window.ESF_CONFIG = {
   webAppUrl: 'https://script.google.com/macros/d/AKfycbzGxzKr_9opy2ZdnJQJ_91GmnR3K3OUqr0_YAsVicNHukMZvZZBrUpfG0nYdMf25bnb/useTrigger',
-  bookingUrl: 'https://calendly.com/gio_chakhidze/new-meeting'
+  bookingUrl: '../booking/free-call.html',
+  freeCallUrl: '../booking/free-call.html',
+  paidBookingUrl: '../booking/paid.html'
 };
