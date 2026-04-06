6f7fafc34a3d8e314d19ee56a4673379f40f3aec
eSellerFit <giorgi.chakhidze@gmail.com>
2026-04-06 15:52:25 -0400

    Update config.js

--- market-pressure/assets/js/config.js
+++ market-pressure/assets/js/config.js
@@ -1,5 +1,5 @@
 window.ESF_CONFIG = {
-  webAppUrl: 'https://hook.us2.make.com/nlz437jbe9lx9f2llujukmpnpdgtyoqv',
+  webAppUrl: 'https://hook.us2.make.com/q0ubv4oi529oasb66k6478bimqd0p83t',
   bookingUrl: '../booking/free-call.html',
   freeCallUrl: '../booking/free-call.html',
   paidBookingUrl: '../booking/paid.html'
